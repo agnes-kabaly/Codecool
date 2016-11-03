e68ba14f9aeab345000cebc5ccd1aa1d0b2c4c2c
agnes-kabaly <alicet.woods@gmail.com>
2016-11-03 01:35:35 +0100

    Update 100door.py

--- 100Doors/100door.py
+++ 100Doors/100door.py
@@ -1,13 +1,18 @@
-door_list = [0 * i for i in range(101)]
+list = [0 * x for x in range(101)]	#Create a list of 100 '0s'
 
-for student in range(1, 101) :
-    for door in range (1, 101) :
-        if Sdoor_list[door] == 0:
-            doorLsit[door] = 1
-            print("Open it!")
+doors = "The following doors are open: "
 
-        else:
-            print("Close it")
-            door_list[door] == 0
+for j in range(1, 101) :
+    for i in range(1, 101) :
+        if i % j == 0:
+            if list[i] == 0:
+                list[i] = 1
+            else:
+                list[i] = 0
 
-print("the following doors are open: ")
+for k in range(100):
+	if list[k] == 1:
+		doors = doors + str(k) + ", "
+
+doors = doors.replace(", , ", "")
+print (doors)
